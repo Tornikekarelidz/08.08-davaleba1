0f61f249ad7a5e6c00e2f06abe31393b4f525057
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-08-09 18:57:59 +0400

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+age = int(input("ramdeni wlis xar?: ")) 
+asaki = age 
+while asaki > 0: 
+    print("Gilocav shen gaxdi",asaki,"wlis") 
+    asaki -= 1
+    if asaki == 8:
+        print("Gilocav goashi shemosvlas")
